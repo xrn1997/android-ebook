d3fefee779c2bc1cde1ccb8f41907631bdf00429
xrn1997 <572249217@qq.com>
2020-05-11 12:08:20 +0800

    更换为腾讯云服务器地址，修复分类标题bug

--- lib_ebook_api/src/main/java/com/ebook/api/config/API.java
+++ lib_ebook_api/src/main/java/com/ebook/api/config/API.java
@@ -4,9 +4,11 @@ package com.ebook.api.config;
 public class API {
     //安卓模拟器访问localhost
 //    public static final String URL_HOST_USER = "http://10.0.2.2:5000/userapi/";
-  //  public static final String URL_HOST_COMMENT="http://10.0.2.2:5000/commentapi/";
+    //  public static final String URL_HOST_COMMENT="http://10.0.2.2:5000/commentapi/";
     //本地局域网IP地址
-    public static final String URL_HOST_USER = "http://192.168.0.102:5000/userapi/";
-    public static final String URL_HOST_COMMENT="http://192.168.0.102:5000/commentapi/";
-
+//    public static final String URL_HOST_USER = "http://192.168.0.102:5000/userapi/";
+//    public static final String URL_HOST_COMMENT="http://192.168.0.102:5000/commentapi/";
+//腾讯云服务器
+        public static final String URL_HOST_USER = "http://175.24.63.162:5000/userapi/";
+    public static final String URL_HOST_COMMENT="http://175.24.63.162:5000/commentapi/";
 }
